9b36465c919cbe970972094db8755131710acaea
Jetsun Prince Torres <59250175+PrensDev@users.noreply.github.com>
2022-04-14 22:02:27 +0800

    Additional Dependencies

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+    Box
+} from '@chakra-ui/react';
+
+class Navbar extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+                <Box>
+                    
+                </Box>
+            </>
+        );
+    }
+}
+
+export default Navbar;
